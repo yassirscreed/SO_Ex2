4bf5efd085bc4cfa727f3e108313df9060ff548b
LeonardoAlvess <106313248+LeonardoAlvess@users.noreply.github.com>
2023-01-15 18:48:50 +0000

    Add files via upload

--- requests.h
+++ requests.h
@@ -1,35 +1,40 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 #include <stdint.h>
+#define BOX_MAX 32
+#define PIPE_MAX 256
+#define MESSAGE_MAX 1024
+#define MAX_BOX_NUMB 64
+#define PIPE_CODE 0777
 
 typedef struct 
 {
     uint8_t code;
-    char named_pipe[256];
-    char box_name[32];
+    char named_pipe[PIPE_MAX];
+    char box_name[BOX_MAX];
 }Register;
 
 typedef struct 
 {
     uint8_t code;
     int32_t return_code;
-    char error_message[1024];
+    char error_message[MESSAGE_MAX];
 }Response;
 
 typedef struct 
 {
     uint8_t code;
     uint8_t last;
-    char box_name[32];
+    char box_name[BOX_MAX];
     uint64_t box_size;
-    uint64_t n_pubs;
-    uint64_t n_subs;
+    uint64_t n_publishers;
+    uint64_t n_subscribers;
 }Manager_list;
 
 typedef struct
 {
     uint8_t code;
-    char message[1024];
+    char message[MESSAGE_MAX];
 }Message;
 
 #endif
